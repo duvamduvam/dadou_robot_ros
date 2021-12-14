790320717e84e07b9bff625a04fa18b15c926cf1
dadou <david@duvam.net>
2021-12-14 20:58:06 +0100

    test audio

--- audio.py
+++ audio.py
@@ -9,6 +9,8 @@ from sound_player import Sound, Playlist, SoundPlayer
 
 class Audio:
 
+    music_thread = {}
+
     @staticmethod
     def play_sound(file):
         pl = Playlist(concurency=2)
@@ -18,6 +20,9 @@ class Audio:
         pl.play()
 
     @staticmethod
-    def play(file):
-        music_thread = Thread(target=Audio.play_sound, args=[file])
-        music_thread.start()
+    def play(self, file):
+        if self.music_thread:
+            self.music_thread.join()
+        else:
+            music_thread = Thread(target=Audio.play_sound, args=[file])
+            music_thread.start()
