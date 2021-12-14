128ffa02779592c5f62ade73a94ef27ea8486309
dadou <david@duvam.net>
2021-12-14 19:57:49 +0100

    test audio

--- audio2.py
+++ audio2.py
@@ -5,24 +5,16 @@ import pygame
 from threading import Thread
 from sound_player import Sound, Playlist, SoundPlayer
 
+
 class Audio2:
 
-    #@staticmethod
-    #def play_sound(file):
-        #logging.info("play sound : "+file)
-        #mixer.init(44100, -16, 2, 2048)
-        #sound = mixer.Sound(file)
-        #sound.play()
-        #while mixer.music.get_busy():
-        #    time.delay(100)
+    @staticmethod
+    def play_sound(file):
+        logging.info("play sound : " + file)
+        sound = Sound(file)
+        sound.play()
 
     @staticmethod
     def play(file):
-        logging.info("play sound : "+file)
-        sound = Sound(file)
-        sound.play()
-        time.sleep(3)
-        #while mixer.music.get_busy():
-        #    time.delay(100)
-        #music_thread = Thread(target=Audio2.play_sound,  args=[file])
-        #music_thread.start()
+        music_thread = Thread(target=Audio2.play_sound, args=[file])
+        music_thread.start()
