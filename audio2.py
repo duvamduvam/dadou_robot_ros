0f9d9be3b06189b725dc031864c3ae2cf361fbcb
dadou <david@duvam.net>
2021-12-14 19:34:01 +0100

    test audio

--- audio2.py
+++ audio2.py
@@ -18,5 +18,11 @@ class Audio2:
 
     @staticmethod
     def play(file):
-        music_thread = Thread(target=Audio2.play_sound,  args=[file])
-        music_thread.start()
+        logging.info("play sound : "+file)
+        mixer.init(44100, -16, 2, 2048)
+        sound = mixer.Sound(file)
+        sound.play()
+        while mixer.music.get_busy():
+            time.delay(100)
+        #music_thread = Thread(target=Audio2.play_sound,  args=[file])
+        #music_thread.start()
