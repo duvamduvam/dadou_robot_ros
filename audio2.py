a1f28315ba68a74de709da1fbb81a2aca57eb3d7
dadou <david@duvam.net>
2021-12-14 19:12:08 +0100

    test audio

--- audio2.py
+++ audio2.py
@@ -8,8 +8,7 @@ class Audio2:
 
     @staticmethod
     def play_sound(file):
-        mixer.pre_init(44100, 16, 2, 4096)
-        mixer.init()
+        mixer.mixer.init(44100, -16, 2, 2048)
         sound = mixer.Sound(file)
         sound.play()
         while mixer.music.get_busy():
