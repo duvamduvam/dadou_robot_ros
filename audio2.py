8cb680caa02f90e79fecb17d0c2147591887f1b0
dadou <david@duvam.net>
2021-12-14 19:45:11 +0100

    test audio

--- audio2.py
+++ audio2.py
@@ -9,12 +9,12 @@ class Audio2:
 
     @staticmethod
     def play_sound(file):
-        logging.info("play sound : "+file)
-        mixer.init(44100, -16, 2, 2048)
-        sound = mixer.Sound(file)
-        sound.play()
-        while mixer.music.get_busy():
-            time.delay(100)
+        #logging.info("play sound : "+file)
+        #mixer.init(44100, -16, 2, 2048)
+        #sound = mixer.Sound(file)
+        #sound.play()
+        #while mixer.music.get_busy():
+        #    time.delay(100)
 
     @staticmethod
     def play(file):
