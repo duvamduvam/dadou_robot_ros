38ad6d4d78ea05d8c65d7d14e7f9b479b72797d0
dadou <david@duvam.net>
2021-12-19 01:40:29 +0100

    update ...

--- utils.py
+++ utils.py
@@ -38,5 +38,6 @@ class Utils:
     @staticmethod
     def is_time(last_time, time_out) -> bool:
         current = round(time.time() * 1000)
+        is_time = (current - last_time) + time_out
         logging.info("last time: " + str(last_time)+" current time : " + str(current) + "time out :" + str(time_out));
-        return (current - last_time) + time_out
+        return is_time
