6d2c1143cc22047852374b5fc0befc392ef81767
dadou <david@duvam.net>
2021-12-20 11:25:54 +0100

    udpate ...

--- python/tests/test_audio.py
+++ python/tests/test_audio.py
@@ -1,17 +1,12 @@
 import unittest
-from python.mapping import Mapping
 from python.audio import Audio
 
 
 class AudioTests(unittest.TestCase):
-    mapping = Mapping()
     audio = Audio()
 
     def play(self):
-        #self.assertEqual(True, False)  # add assertion here
-        audio_path = self.mapping.get_audio_file("A5")
-        if audio_path:
-            self.audio.play_sound(audio_path)
+        self.execute("A5")
 
 
 if __name__ == '__main__':
