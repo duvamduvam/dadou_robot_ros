63d94aec51978a37391df510229d3235ad8f1d4f
dadou <david@duvam.net>
2021-12-20 17:26:58 +0100

    udpate ...

--- python/tests/test_neck.py
+++ python/tests/test_neck.py
@@ -15,7 +15,6 @@ class NeckTests(unittest.TestCase):
     def test_move_key(self):
         logging.debug("start test servo")
         for i in range(3):
-            key = '!'
             logging.debug("test key 60 for servo")
             self.neck.update(40)
             self.neck.process()
@@ -26,7 +25,6 @@ class NeckTests(unittest.TestCase):
             time.sleep(1)
             self.neck.process()
             time.sleep(5)
-            key = '~'
             logging.debug("test key 130 for servo")
             self.neck.update(100)
             self.neck.process()
@@ -37,7 +35,6 @@ class NeckTests(unittest.TestCase):
             time.sleep(1)
             self.neck.process()
             time.sleep(5)
-
 
 
 if __name__ == '__main__':
