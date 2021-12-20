e785766c2a6f269025c606144cb14f93a8827891
dadou <david@duvam.net>
2021-12-20 19:13:56 +0100

    udpate ...

--- python/lights.py
+++ python/lights.py
@@ -10,6 +10,9 @@ from rainbowio import colorwheel
 
 
 class Lights:
+
+    logging.config.fileConfig(fname='logging.conf', disable_existing_loggers=False)
+    
     # LED strip configuration:
     LED_COUNT = 250  # Number of LED pixels.
     LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
