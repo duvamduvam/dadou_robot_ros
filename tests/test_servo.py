259367f16a56b6edd83992bb22803959a6d6b8ac
dadou <david@duvam.net>
2021-12-20 10:35:52 +0100

    udpate ...

--- tests/test_servo.py
+++ tests/test_servo.py
@@ -1,21 +1,44 @@
 import time
 import unittest
 import logging
+import logging.config
 
 from head import Head
 
 
 class Servo(unittest.TestCase):
-    logging.basicConfig(level=logging.DEBUG)
+    logging.config.fileConfig(fname='logging.conf', disable_existing_loggers=False)
 
     head = Head()
 
-    def move_key(self):
-        for i in range(0, 10, 1):
+    def test_move_key(self):
+        head = Head()
+        logging.debug("start test servo")
+        for i in range(3):
             key = '!'
-            self.head.update(key)
+            logging.debug("test key 60 for servo")
+            head.update(40)
+            head.process()
+            time.sleep(1)
+            head.process()
+            time.sleep(1)
+            head.process()
+            time.sleep(1)
+            head.process()
             time.sleep(5)
             key = '~'
-            self.head.update(key)
+            logging.debug("test key 130 for servo")
+            head.update(100)
+            head.process()
+            time.sleep(1)
+            head.process()
+            time.sleep(1)
+            head.process()
+            time.sleep(1)
+            head.process()
             time.sleep(5)
 
+
+
+if __name__ == '__main__':
+    unittest.main()
