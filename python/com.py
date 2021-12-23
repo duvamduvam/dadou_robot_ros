511c6d0e1564006483f2aea822ebc36912d44d9e
dadou <david@duvam.net>
2021-12-23 22:25:08 +0100

    udpate ...

--- python/com.py
+++ python/com.py
@@ -9,6 +9,7 @@ class Com:
     """A simple example class"""
     i = 12345
 
+    arduino = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
     watcher = FileWatcher()
 
     def get_msg(self):
@@ -16,22 +17,10 @@ class Com:
         if self.watcher.changed():
             return self.watcher.get_last_key()
 
-        print('Running. Press CTRL-C to exit.')
-        with serial.Serial("/dev/ttyAMA0", 9600, timeout=1) as arduino:
-            time.sleep(0.1)  # wait for serial to open
-            if arduino.isOpen():
-                print("{} connected!".format(arduino.port))
-                try:
-                    while True:
-                        cmd = input("Enter command : ")
-                        arduino.write(cmd.encode())
-                        # time.sleep(0.1) #wait for arduino to answer
-                        while arduino.inWaiting() == 0:
-                            pass
-                        if arduino.inWaiting() > 0:
-                            answer = arduino.readline()
-                            print(answer)
-                            arduino.flushInput()  # remove data after reading
-                            logging.info('received from arduino'+answer)
-                except KeyboardInterrupt:
-                    print("KeyboardInterrupt has been caught.")
+        time.sleep(0.1)  # wait for serial to open
+        if self.arduino.isOpen():
+            print("{} connected!".format(self.arduino.port))
+            if self.arduino.inWaiting() > 0:
+                answer = self.arduino.readline()
+                self.arduino.flushInput()  # remove data after reading
+                logging.info('received from arduino' + answer)
