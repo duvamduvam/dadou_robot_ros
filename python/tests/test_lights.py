4a6f3ac260733b9ca399d8ca7bfeeb854a6caf55
dadou <david@duvam.net>
2021-12-25 12:02:05 +0100

    udpate ...

--- python/tests/test_lights.py
+++ python/tests/test_lights.py
@@ -4,10 +4,14 @@ import unittest
 import board
 import neopixel
 from adafruit_led_animation import helper
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.chase import Chase
 from adafruit_led_animation.animation.comet import Comet
 from adafruit_led_animation.color import AMBER
+from adafruit_led_animation.group import AnimationGroup
 from adafruit_led_animation.helper import PixelMap
-
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation import color
 from python.actions.lights import Lights
 
 
@@ -22,15 +26,14 @@ class LightsTest(unittest.TestCase):
 
     lights = Lights()
 
-
     def test_color_chase(self):
-        pixels = neopixel.NeoPixel(board.D18, 8*6*8, auto_write=False)
+        pixels = neopixel.NeoPixel(board.D18, 8 * 6 * 8, auto_write=False)
         pixels.brightness = 0.1
-        #pixel_wing_vertical = helper.PixelMap.vertical_lines(
+        # pixel_wing_vertical = helper.PixelMap.vertical_lines(
         #    pixels, 8, 8, helper.horizontal_strip_gridmap(8, alternating=False)
-        #)
+        # )
 
-        #comet_v = Comet(pixel_wing_vertical, speed=0.1, color=AMBER, tail_length=6, bounce=True)
+        # comet_v = Comet(pixel_wing_vertical, speed=0.1, color=AMBER, tail_length=6, bounce=True)
 
         m1 = PixelMap(pixels, [(0, 64)])
         m2 = PixelMap(pixels, [(64, 128)])
@@ -46,16 +49,33 @@ class LightsTest(unittest.TestCase):
         m5[0] = (255, 0, 255)
         m6[0] = (255, 0, 255)
 
-        m1.show()
-        m2.show()
-        m3.show()
-        m4.show()
-        m5.show()
-        m6.show()
+        animations = AnimationSequence(
+            # Synchronized to 0.5 seconds. Ignores the second animation setting of 3 seconds.
+            AnimationGroup(
+                Blink(m1, 0.5, color.CYAN),
+                Blink(m2, 3.0, color.AMBER),
+                sync=True,
+            ),
+            # Different speeds
+            AnimationGroup(
+                Comet(m3, 0.1, color.MAGENTA, tail_length=5),
+                Comet(m4, 0.01, color.MAGENTA, tail_length=15),
+            ),
+            # Different animations
+            AnimationGroup(
+                Blink(m5, 0.5, color.JADE),
+                Comet(m6, 0.05, color.TEAL, tail_length=15),
+            ),
+            # Sequential animations on the built-in NeoPixels then the NeoPixel strip
+            Chase(m1, 0.05, size=2, spacing=3, color=color.PURPLE),
+            Chase(m2, 0.05, size=2, spacing=3, color=color.PURPLE),
+            advance_interval=3.0,
+            auto_clear=True,
+            auto_reset=True,
+        )
 
-
-        #while True:
-        #    comet_v.animate()
+        while True:
+            animations.animate()
 
     @unittest.skip
     def test_rainbow_cycle(self):
