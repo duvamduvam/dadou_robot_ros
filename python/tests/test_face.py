7d879000ed36cd91081b276bef6c020344985b02
dadou <david@duvam.net>
2021-12-26 19:21:57 +0100

    udpate ...

--- python/tests/test_face.py
+++ python/tests/test_face.py
@@ -19,10 +19,10 @@ class MyTestCase(unittest.TestCase):
     json_manager = JsonManager()
     face = Face()
 
+    @unittest.skip
     def test_basic(self):
         pixels = neopixel.NeoPixel(board.D18, 8 * 6 * 8, auto_write=False)
         pixels.brightness = 0.1
-
         while True:
             # Comment this line out if you have RGBW/GRBW NeoPixels
             pixels[5] = (255, 0, 0)
@@ -33,17 +33,18 @@ class MyTestCase(unittest.TestCase):
             pixels.show()
             time.sleep(1)
 
-    @unittest.skip
+
     def test_img_mouth(self):
         logging.info("test face")
         logging.info(self.face.visuals[0].rgb)
         path = self.json_manager.get_visual_path("eye-still")
         visual = Visual("eye-still", path)
         logging.info(visual.rgb)
-        self.face.pixels[10] = visual.rgb[2][2]
+        #self.face.pixels[10] = visual.rgb[2][2]
         self.face.pixels.fill((255, 0, 0))
-        logging.info("test 1 pixel")
-        logging.info(visual.rgb[2][2])
+        self.face.pixels.show()
+        #logging.info("test 1 pixel")
+        #logging.info(visual.rgb[2][2])
         # self.face.fill_matrix(0, 64, visual)
         # self.face.pixels.show()
         time.sleep(100)
