74e31faf21b1fcbf656faf6d506dde561431b767
dadou <david@duvam.net>
2021-12-27 00:47:22 +0100

    udpate ...

--- python/tests/test_face.py
+++ python/tests/test_face.py
@@ -20,8 +20,10 @@ class MyTestCase(unittest.TestCase):
     face = Face()
 
     def test_img_eye(self):
+        logging.info("animate face A1")
         self.face.update("A1")
         while True:
+            logging.info("animate")
             self.face.animate()
 
     @unittest.skip
