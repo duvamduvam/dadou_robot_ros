a12a9f476510fec70e3927a76f2d187b39ec4940
duvamduvam <david@mail.duvam.net>
2021-12-29 15:42:35 +0100

    udpate ...

--- image_mapping.py
+++ image_mapping.py
@@ -12,8 +12,9 @@ class ImageMapping:
     def mapping(self, pixels, image):
         for y in range(len(image)):
             for x in range(len(image[y])):
+
                 index = (x % self.matrix_width) + ((x // self.matrix_width) * (
                         self.matrix_width * self.matrix_height)) + (
-                                (y // 2) * self.matrix_width)
+                    (y * self.matrix_width))
                 logging.debug("pixel[" + str(index) + "] = image[" + str(y) + "][" + str(x) + "]")
                 pixels[index] = image[y][x]
