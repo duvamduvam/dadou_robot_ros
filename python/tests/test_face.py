8a585888cf140502e9bac2e7ae89384823eab8db
duvamduvam <david@mail.duvam.net>
2021-12-29 15:05:01 +0100

    udpate ...

--- python/tests/test_face.py
+++ python/tests/test_face.py
@@ -37,14 +37,14 @@ class TestFace(unittest.TestCase):
         self.face.pixels.fill(BLACK)
         visual = Visual.get_visual("mopen1", self.face.visuals)
         #self.face.fill_matrix(0, self.face.mouth_end, visual)
-        self.face.pixels[64] = AMBER
-        self.face.pixels[65] = AMBER
-        self.face.pixels[66] = AMBER
-        self.face.pixels[67] = AMBER
-        self.face.pixels[68] = AMBER
-        self.face.pixels[69] = AMBER
-        self.face.pixels[70] = AMBER
-        self.face.pixels[71] = AMBER
+        self.face.pixels[(8*3)+64] = AMBER
+        self.face.pixels[(8*3)+65] = AMBER
+        self.face.pixels[(8*3)+66] = AMBER
+        self.face.pixels[(8*3)+67] = AMBER
+        self.face.pixels[(8*3)+68] = AMBER
+        self.face.pixels[(8*3)+69] = AMBER
+        self.face.pixels[(8*3)+70] = AMBER
+        self.face.pixels[(8*3)+71] = AMBER
         self.face.pixels[220] = RED
 
         self.face.pixels.show()
