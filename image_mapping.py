5cccdbde3d80e0ab058fb9a388e4f61c3ea2fd63
duvamduvam <david@mail.duvam.net>
2021-12-31 14:57:45 +0100

    udpate ...

--- image_mapping.py
+++ image_mapping.py
@@ -32,7 +32,7 @@ class ImageMapping:
     def inverse_bottom_image(image):
         new_image = image
 
-        for y in range(int(len(image) / 2), len(image) - 1):
+        for y in range(int(len(image) / 2), len(image)):
             for x in range(0, len(image[0])):
                 ypos = len(image) + (int(len(image) / 2) - (y + 1))
                 xpos = (len(image[0]) - 1) - x
