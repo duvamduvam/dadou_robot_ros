553a55e0dd688b93b56a04b846783bae3e2dfdab
duvamduvam <david@mail.duvam.net>
2022-01-02 01:37:23 +0100

    udpate ...

--- python/tests/test_face.py
+++ python/tests/test_face.py
@@ -2,6 +2,7 @@ import logging.config
 
 from image_mapping import ImageMapping
 from python.tests.conf_test import TestSetup
+
 TestSetup()
 
 import time
@@ -28,33 +29,32 @@ class TestFace(unittest.TestCase):
         visual = Visual.get_visual("test", self.face.visuals)
         self.image_mapping.mapping(self.face.pixels, visual.rgb)
         self.face.pixels.show()
-        #time.sleep(10)
+        # time.sleep(10)
 
     @unittest.skip
     def test_img_mouth(self):
         logging.info("test_img_mouth")
         self.face.pixels.fill(BLACK)
         visual = Visual.get_visual("mopen1", self.face.visuals)
-        #self.face.fill_matrix(0, self.face.mouth_end, visual)
-        self.face.pixels[(8*8*2)] = AMBER
-        self.face.pixels[(8*8*2)+1] = AMBER
-        self.face.pixels[(8*8*2)+2] = AMBER
-        self.face.pixels[(8*8*2)+3] = AMBER
-        self.face.pixels[(8*8*2)+4] = AMBER
-        self.face.pixels[(8*8*2)+5] = AMBER
-        self.face.pixels[(8*8*2)+6] = AMBER
-        self.face.pixels[(8*8*2)+7] = AMBER
+        # self.face.fill_matrix(0, self.face.mouth_end, visual)
+        self.face.pixels[(8 * 8 * 2)] = AMBER
+        self.face.pixels[(8 * 8 * 2) + 1] = AMBER
+        self.face.pixels[(8 * 8 * 2) + 2] = AMBER
+        self.face.pixels[(8 * 8 * 2) + 3] = AMBER
+        self.face.pixels[(8 * 8 * 2) + 4] = AMBER
+        self.face.pixels[(8 * 8 * 2) + 5] = AMBER
+        self.face.pixels[(8 * 8 * 2) + 6] = AMBER
+        self.face.pixels[(8 * 8 * 2) + 7] = AMBER
         self.face.pixels[220] = RED
 
         self.face.pixels.show()
         time.sleep(10)
 
-
     def test_face2(self):
         logging.info("animate face A1")
         self.face.update("A1")
         while True:
-            #logging.info("animate")
+            # logging.info("animate")
             self.face.animate()
 
     @unittest.skip
@@ -75,7 +75,7 @@ class TestFace(unittest.TestCase):
     def test_img_eye(self):
         path = self.json_manager.get_visual_path("eye-still")
         visual = Visual("eye-still", path)
-        #self.face.pixels[10] = visual.rgb[2][2]
+        # self.face.pixels[10] = visual.rgb[2][2]
         self.face.pixels.fill((0, 0, 0))
         self.face.pixels.show()
         self.face.fill_matrix(0, 64, visual)
