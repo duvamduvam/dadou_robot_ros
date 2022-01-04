89020ea572c3514233147c64de3cc99292d68b7c
duvamduvam <david@mail.duvam.net>
2022-01-04 16:27:32 +0100

    udpate ...

--- python/actions/audio.py
+++ python/actions/audio.py
@@ -2,8 +2,10 @@
 #https://github.com/Krozark/sound-player/blob/master/example.py
 import logging
 
-from python.mapping import Mapping
 from sound_player import Sound, Playlist, SoundPlayer
+
+from python.json_manager import JsonManager
+from python.path_time import PathTime
 
 
 class Audio:
@@ -15,11 +17,12 @@ class Audio:
     player = SoundPlayer()
     silence = "audios/silence.wav"
 
-    def __init__(self, mapping: Mapping):
-        self.mapping = mapping
+    def __init__(self, json_manager: JsonManager):
+        self.json_manager = json_manager
 
     def play_sounds(self, audios):
         self.player.stop()
+
         for audio in audios:
             logging.info("enqueue: " + audio.get_path())
             self.player.enqueue(Sound(audio.get_path()), 1)
@@ -31,6 +34,16 @@ class Audio:
         self.player.stop()
 
     def process(self, key):
-        audio_path = self.mapping.get_audios(key)
-        self.play_sounds(audio_path)
+        audio_sequence = self.json_manager.get_audios(key)
+        audios = []
+        for seq in audio_sequence:
+            logging.debug("iterate key : " + seq['key'])
+            if seq['key'] == key:
+                logging.debug("found key : " + key)
+                for audio_seq in seq['sequence']:
+                    audio_path = self.get_audio_path_by_name(audio_seq['name'])
+                    logging.debug("audios name : " + audio_seq['name'] + " path : " + audio_path)
+                    audios.append(PathTime(audio_path, audio_seq['wait']))
+
+        self.play_sounds(audios)
         logging.info("update")
--- python/json_manager.py
+++ python/json_manager.py
@@ -6,55 +6,81 @@ import jsonpath_rw_ext
 
 
 class JsonManager:
-    with open("json/colors.json", 'r') as json_file:
+    JSON_PATH = "json/"
+    AUDIOS = "audios.json"
+    AUDIO_SEQUENCE = "audio_sequence.json"
+    COLORS = "colors.json"
+    FACE_SEQUENCE = "face_sequence.json"
+    LIGHTS = "lights.json"
+    LIGHTS_SEQUENCE = "lights_sequence.json"
+    VISUALS = "visuals.json"
+
+    with open(JSON_PATH + COLORS, 'r') as json_file:
         colors = json.load(json_file)
 
-    with open("json/face_sequence.json", 'r') as json_file:
+    with open(JSON_PATH + FACE_SEQUENCE, 'r') as json_file:
         face_seq = json.load(json_file)
 
-    with open("json/lights_sequence.json", 'r') as json_file:
+    with open(JSON_PATH + LIGHTS, 'r') as json_file:
         lights = json.load(json_file)
 
-    with open("json/visual.json", 'r') as json_file:
+    with open(JSON_PATH + VISUALS, 'r') as json_file:
         visual = json.load(json_file)
+
+    with open(JSON_PATH + AUDIOS) as json_file:
+        audios = json.load(json_file)
+
+    with open(JSON_PATH + AUDIO_SEQUENCE) as json_file:
+        audio_seq = json.load(json_file)
+
+    @staticmethod
+    def standard_return(result, first, key, json_file):
+        logging.debug(result)
+        to_return = {}
+        error = False
+
+        if not bool(result):
+            error = True
+        else:
+            if first:
+                if len(result) > 0:
+                    to_return = result[0]
+                else:
+                    error = True
+            else:
+                to_return = result
+            if key:
+                to_return = to_return[key]
+
+        if not error:
+            return to_return
+        else:
+            logging.error("no data for " + key + " in " + json_file)
+        return 0
 
     def get_visual_path(self, key) -> str:
         result = jsonpath_rw_ext.match('$.visual[?name==' + key + ']', self.visual)
-        if len(result) == 0:
-            logging.error("no visual path for " + key)
-            return
-        logging.debug(result)
-        return result[0]['path']
+        return self.standard_return(result, True, 'path', self.VISUALS)
 
     def get_face_part(self, name) -> str:
         result = jsonpath_rw_ext.match('$.part_seq[?name==' + name + ']', self.face_seq)
-        if len(result) == 0:
-            logging.error("no visual path for " + name)
-            return
-        logging.debug(result)
-        return result[0]
+        return self.standard_return(result, False, 'path', self.FACE_SEQUENCE)
 
     def get_all_visual(self):
         result = jsonpath_rw_ext.match('$.visual[*]', self.visual)
-        logging.debug(result)
-        return result
+        return self.standard_return(result, False, False, self.VISUALS)
 
     def get_face_seq(self, key):
         result = jsonpath_rw_ext.match('$.main_seq[?keys~' + key + ']', self.face_seq)
-        logging.debug(result)
-        return result[0]
+        return self.standard_return(result, True, False, self.FACE_SEQUENCE)
 
     def get_part_seq(self, name):
         result = jsonpath_rw_ext.match('$.part_seq[?name==' + name + ']', self.face_seq)
-        logging.debug(result)
-        return result[0]
+        return self.standard_return(result, True, False, self.FACE_SEQUENCE)
 
     def get_lights(self, key):
         result = jsonpath_rw_ext.match('$.lights_seq[?keys~' + key + ']', self.lights)
-        logging.debug(result)
-        if len(result) > 0:
-            return result[0]
-        return 0
+        return self.standard_return(result, True, False, self.LIGHTS_SEQUENCE)
 
     def get_color(self, key):
         result = jsonpath_rw_ext.match('$.colors[?name~' + key + ']', self.colors)
@@ -64,6 +90,7 @@ class JsonManager:
             color = (int(json_color['red']), int(json_color['green']), int(json_color['blue']))
             return color
         else:
+            logging.error("no color" + key)
             return 0
 
     @staticmethod
@@ -71,4 +98,28 @@ class JsonManager:
         if key in json_object:
             return json_object[key]
         else:
+            logging.error("no attribut" + key)
             return 0
+
+    def get_audio_path_by_name(self, name) -> str:
+        result = jsonpath_rw_ext.match('$.audios[?name~' + name + ']', self.audios)
+        return self.standard_return(result, True, False, self.AUDIOS)
+
+    def get_audios(self, key: str) -> str:
+        result = jsonpath_rw_ext.match('$.audios_seq[?name~' + key + ']', self.audios)
+        return self.standard_return(result, True, False, self.AUDIO_SEQUENCE)
+
+
+
+        """
+        audios = []
+        for seq in self.audio_sequence:
+            logging.debug("iterate key : " + seq['key'])
+            if seq['key'] == key:
+                logging.debug("found key : " + key)
+                for audio_seq in seq['sequence']:
+                    audio_path = self.get_audio_path_by_name(audio_seq['name'])
+                    logging.debug("audios name : " + audio_seq['name'] + " path : " + audio_path)
+                    audios.append(PathTime(audio_path, audio_seq['wait']))
+        return audios
+        """
