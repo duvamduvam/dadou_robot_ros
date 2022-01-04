c4907d7b25ddcf833e632e43dacd2be7fa57de56
duvamduvam <david@mail.duvam.net>
2022-01-05 00:34:14 +0100

    udpate ...

--- python/tests/test_com.py
+++ python/tests/test_com.py
@@ -7,13 +7,14 @@ from python.utils import Utils
 
 TestSetup()
 
+
 class MyTestCase(unittest.TestCase):
 
     com = Com()
 
     def test_com(self):
         start_time = Utils.current_milli_time()
-        if Utils.is_time(start_time, 200000):
+        while Utils.is_time(start_time, 200000):
             msg = self.com.get_msg()
             if msg:
                 logging.info("new msg : ".msg)
