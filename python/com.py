09cef364200d21e9ca3842063a0079a79c009cfd
duvamduvam <david@mail.duvam.net>
2022-01-05 01:50:50 +0100

    udpate ...

--- python/com.py
+++ python/com.py
@@ -7,8 +7,8 @@ from python.file_watcher import FileWatcher
 
 class Com:
     arduino_enable = True
-    #arduino = serial.Serial("/dev/serial0", 115200, timeout=1)
-    arduino = serial.Serial("/dev/ttyAMA0", 115200, timeout=1)
+    arduino = serial.Serial("/dev/serial0", 115200, timeout=1)
+    #arduino = serial.Serial("/dev/ttyAMA0", 115200, timeout=1)
     #arduino = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)
     # time.sleep(0.1)  # wait for serial to open
     watcher = FileWatcher()
