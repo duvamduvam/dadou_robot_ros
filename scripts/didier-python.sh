8fcd223a7d249736ab5546f1c6c7d0a4e42502f4
duvamduvam <david@mail.duvam.net>
2022-01-09 00:27:13 +0100

    udpate ...

--- scripts/didier-python.sh
+++ scripts/didier-python.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo rm -rf /home/pi/deploy/*
-git clone https://github.com/duvamduvam/didier-python.git
 git config user.name toto
 git config user.amil toto@toto.com
 git config pull.ff only
