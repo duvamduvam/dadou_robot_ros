248c3ac927f418647bff0209a0cdd4b2d84e7b09
duvamduvam <david@mail.duvam.net>
2022-01-08 20:02:55 +0100

    udpate ...

--- python/input/message.py
+++ python/input/message.py
@@ -12,7 +12,7 @@ class Message:
 
     def __init__(self, left_wheel, right_wheel, neck, key):
         logging.info(
-            "new message left wheel : " + left_wheel + " right_wheel : " + right_wheel + " head : " + head + " key : " + key)
+            "new message left wheel : " + left_wheel + " right_wheel : " + right_wheel + " head : " + neck + " key : " + key)
         self.left_wheel = left_wheel
         self.right_wheel = right_wheel
         self.neck = neck
