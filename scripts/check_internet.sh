012c9c99b573c717cd6a50155a0b9b2ab90de39e
duvamduvam <david@mail.duvam.net>
2022-01-08 19:27:37 +0100

    udpate ...

---
+++ scripts/check_internet.sh
@@ -0,0 +1,6 @@
+#check internet
+internet=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+if [ "$internet" != "ok" ]; then
+  echo "no internet..."
+  exit
+fi
--- scripts/push_from_home.sh
+++ scripts/push_from_home.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-
+#!/bin/bash
+/home/pi/scripts/check_internet.sh
 ssh home '~/Nextcloud/rosita/python/didier-python/scripts/push-didier.sh'
 cd /home/pi/deploy/didier-python
 git pull origin
