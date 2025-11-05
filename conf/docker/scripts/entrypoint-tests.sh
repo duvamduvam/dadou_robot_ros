389cb7acabcf1fd3e95f191ac03a4e42b2b6f1a5
dadou <david@duvam.net>
2025-11-05 20:58:35 +0100

    Make test entrypoint compatible with set -u

--- conf/docker/scripts/entrypoint-tests.sh
+++ conf/docker/scripts/entrypoint-tests.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
+# ROS setup scripts reference optional environment variables that may not be defined
+# when running with `set -u`, so temporarily relax nounset while sourcing them.
+set +u
 source /opt/ros/humble/setup.bash
 source /home/ros2_ws/src/robot/conf/scripts/setup-robot-env.sh
+set -u
 
 exec "$@"
