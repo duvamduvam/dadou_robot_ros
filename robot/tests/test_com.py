7407dfbedfb284be6ac8c2816efe51277f2609ed
dadou <david@duvam.net>
2025-11-05 23:01:52 +0100

    update

--- robot/tests/test_com.py
+++ robot/tests/test_com.py
@@ -1,12 +1,5 @@
 import unittest
 
-from dadoucontrol import Com
-from dadoucontrol import Utils
-from tests import TestSetup
-
-TestSetup()
-
-
 class JenkinsTestCase(unittest.TestCase):
 
     def test_upper(self):
