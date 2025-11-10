93cc259f141e61acc462b558dc3f4164df3edfb3
dadou <david@duvam.net>
2025-11-10 23:57:52 +0100

    update

--- robot/tests/test_com.py
+++ robot/tests/test_com.py
@@ -2,5 +2,8 @@ import unittest
 
 class JenkinsTestCase(unittest.TestCase):
 
-    def test_upper(self):
+    def test_true(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_false(self):
+        self.assertEqual('foo'.upper(), 'boo')
